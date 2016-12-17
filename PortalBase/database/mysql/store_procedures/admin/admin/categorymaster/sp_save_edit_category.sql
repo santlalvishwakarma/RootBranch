f026b41790f59be2efa6941226200ebb3919ba73
deepaknaithani88 <deepaknaithani88@gmail.com>
2016-12-17 19:07:55 +0530

    Added is_active flag in insert clause

--- PortalBase/database/mysql/store_procedures/admin/admin/categorymaster/sp_save_edit_category.sql
+++ PortalBase/database/mysql/store_procedures/admin/admin/categorymaster/sp_save_edit_category.sql
@@ -43,7 +43,7 @@ BEGIN
 		INSERT INTO category_master(category_code, category_name, category_description, seo_title, seo_keyword, seo_description, image_url,
 									is_active, created_by, modified_by, created_date, modified_date)
 							 VALUES(p_product_category_code, p_product_category_name, p_product_category_description, p_seo_title,
-							 		p_seo_keyword, p_seo_description, p_image_url, p_user_login, p_user_login, NOW(), NOW());
+							 		p_seo_keyword, p_seo_description, p_image_url, p_is_active, p_user_login, p_user_login, NOW(), NOW());
 		
 		SELECT LAST_INSERT_ID() INTO v_last_inserted_id;
 
