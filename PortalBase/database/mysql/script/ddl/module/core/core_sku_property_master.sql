a2c0bcc5f124c6fb2973b7389a22376433196bb7
Deepak Naithani <deepaknaithani@Deepaks-MacBook-Pro.local>
2017-03-21 07:33:18 +0530

    Modified columns name, datatypes and references in core_sku_property_master

--- PortalBase/database/mysql/script/ddl/module/core/core_sku_property_master.sql
+++ PortalBase/database/mysql/script/ddl/module/core/core_sku_property_master.sql
@@ -1,14 +1,15 @@
-CREATE TABLE core_sku_property_master(
-									  sku_property_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-									  sku_property_code VARCHAR(25) DEFAULT NULL,
-									  sku_property_name VARCHAR(25) DEFAULT NULL,
-									  sku_property_description VARCHAR(25) DEFAULT NULL,
-									  sku_property_type VARCHAR(25) DEFAULT NULL,
-									  sku_property_price INT(10) UNSIGNED DEFAULT NULL,
-									  is_active TINYINT(1) NOT NULL DEFAULT '0',
-									  created_by VARCHAR(50) NOT NULL,
-									  created_date DATETIME NOT NULL,
-									  modified_by VARCHAR(50) NOT NULL,
-									  modified_date DATETIME NOT NULL,
-									  PRIMARY KEY (sku_property_id))
-									  ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE core_sku_property_master (sku_property_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+									   sku_property_code varchar(25) DEFAULT NULL,
+									   sku_property_name varchar(25) DEFAULT NULL,
+									   sku_property_description varchar(25) DEFAULT NULL,
+									   sku_property_type int(10) unsigned DEFAULT NULL,
+									   sku_property_price int(10) unsigned DEFAULT NULL,
+									   is_active tinyint(1) NOT NULL DEFAULT '0',
+									   created_by varchar(50) NOT NULL,
+									   created_date datetime NOT NULL,
+									   modified_by varchar(50) NOT NULL,
+									   modified_date datetime NOT NULL,
+									   PRIMARY KEY (sku_property_id),
+									   KEY fk_sku_property_type_parameter (sku_property_type),
+									   CONSTRAINT fk_sku_property_type_parameter FOREIGN KEY (sku_property_type) REFERENCES core_parameter_master (parameter_id))
+									   ENGINE=InnoDB DEFAULT CHARSET=utf8;
