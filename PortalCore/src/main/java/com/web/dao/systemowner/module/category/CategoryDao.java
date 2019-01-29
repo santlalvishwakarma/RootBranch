008ebb77a06d6dc291d277995b31bbeaa9a7e4f5
santlalvishwakarma <vsantlal@yahoo.com>
2019-01-30 01:09:05 +0530

    fixed bugs

--- PortalCore/src/main/java/com/web/dao/systemowner/module/category/CategoryDao.java
+++ PortalCore/src/main/java/com/web/dao/systemowner/module/category/CategoryDao.java
@@ -3,7 +3,9 @@ package com.web.dao.systemowner.module.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.web.model.dvo.CategoryDVO;
+
 @Repository
-public interface CategoryDao extends JpaRepository<CategoryDao, Integer> {
+public interface CategoryDao extends JpaRepository<CategoryDVO, Integer> {
 
 }
